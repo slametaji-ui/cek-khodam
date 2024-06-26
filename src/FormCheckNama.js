38f09827bc9fd1a8178eae402b5a454fc974ca61
slamet aji <slametaji.info@gmail.com>
2024-06-26 15:13:57 +0700

    opacity

--- src/FormCheckNama.js
+++ src/FormCheckNama.js
@@ -34,10 +34,10 @@ const FormCheckNama = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 bg-cover bg-center" style={{ backgroundImage: `url('https://1.bp.blogspot.com/-VCgDPXRPNl4/VtbL_C0LUiI/AAAAAAAACD0/TBXxiI2nycY/s1600/asal%2Busul%2Bnyi%2Broro%2Bkidul.JPG')` }}>
-      <div className="flex flex-col items-center bg-white bg-opacity-15 p-9 shadow-lg pt-4 mt-4 text-center rounded-xl py-4 px-4">
+      <div className="flex flex-col items-center bg-white bg-opacity-80 p-9 shadow-lg pt-4 mt-4 text-center rounded-xl py-4 px-4">
       <img src="https://png.pngtree.com/png-clipart/20230401/original/pngtree-tiger-funny-png-image_9014537.png" alt="Pertamina Logo" className="mb-2 h-24" />
         <h1 className="text-2xl font-bold mb-8 px-28 text-center">Cek Khodam</h1>
-        <h2 className="text-lg font-medium mt-2">
+        <h2 className="text-lg font-medium mb-2">
                     Cek Khodam yang ada di dalam diri kamu
                 </h2>
         <form onSubmit={handleSubmit} className="w-full max-w-xl">
