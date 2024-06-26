79aa6022f7b35ef7314e0ec982666dd5e6740b78
slamet aji <slametaji.info@gmail.com>
2024-06-26 14:55:03 +0700

    result

--- src/ResultCheckNama.js
+++ src/ResultCheckNama.js
@@ -8,9 +8,17 @@ const ResultCheckNama = ({ data }) => {
 
     return (
         <div className="border-t pt-4 mt-4 text-center bg-white rounded-xl py-4 px-4">
-            <p className="text-gray-600">{`${capitalize(data.nama)}`}</p>
-            <h2 className="text-lg font-medium mt-2">Khodam</h2>
-            <p className="text-gray-600 text-lg font-medium">{`${capitalize(data.data)}`}</p>
+            <p className="text-gray-600">{capitalize(data.nama)}</p>
+            {data.data === "tidak ada" ? (
+                <h2 className="text-lg font-medium mt-2">
+                    Yahh kamu ga punya khodam 😰😰, sepertinya kamu harus isi khodam
+                </h2>
+            ) : (
+                <>
+                    <h2 className="text-lg font-medium mt-2">Wihh Keren Khodam kamu</h2>
+                    <p className="text-gray-600 text-lg font-medium">{capitalize(data.data)}</p>
+                </>
+            )}
         </div>
     );
 };
